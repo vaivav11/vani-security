49fa9e8eafd1cc3c0bfa717d64c98f0268b04b0a
Vaiva Margeleviciute <vaivav11@gmail.com>
2018-03-30 20:40:49 +0100

    added static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
-  def home
+  def show
+    render template: "pages/#{params[:page]}"
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#services', as: :services
+  get 'careers', to: 'pages#careers', as: :careers
+  get 'news', to: 'pages#news', as: :news
+  get 'contact_us', to: 'pages#contact_us', as: :contact_us
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
